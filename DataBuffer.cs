e77c5ba3385c9da201b463b60af149aa948d04c8
sudarshan1975 <bharadwaj.sp@gmail.com>
2026-07-11 09:44:06 -0500

    Incremental changes. Code still doesn't build.

---
+++ DataBuffer.cs
@@ -0,0 +1,376 @@
+﻿using Serilog;
+
+namespace DCTRClasses
+{
+    /// <summary>
+    /// This class allows loading of line data in a buffered fashion
+    /// Line data are sometimes spread across many files (for ex. - the CDSD
+    /// database contains more than 2,500 files, with data for around 700 million lines)
+    /// The DataBuffer class keeps track of files within each spectral sub-interval and
+    /// loads files upon demand
+    /// </summary>
+    public class DataBuffer
+    {
+        #region Public Methods
+
+        // Convenience method to get the names of files represented by the buffer
+        public List<string> GetFileList()
+        {
+            return [.. _fileList.Select(item => item.FileName)];
+        }
+
+        /// <summary>
+        /// Gives the starting and ending indices of files corresponding
+        /// to the specified start and end wavenumbers
+        /// </summary>
+        /// <param name="indexList"></param>
+        /// <param name="startWNum"></param>
+        /// <param name="endWNum"></param>
+        public void GetNextIndexList(int[] indexList, double startWNum, double endWNum)
+        {
+            if (LineEnsemble == null || indexList[1] >= LineEnsemble.LineCount - 1)
+            {
+                UpdateData();
+            }
+            else
+            {
+                if (LineEnsemble.DataArray[(indexList[1] + 1) * LineEnsemble.NDataColumns] >= endWNum)
+                {
+                    indexList[0] = -1;
+                    indexList[1] = -1;
+
+                    return;
+                }
+            }
+
+            indexList[0] = -1;
+            indexList[1] = -1;
+
+            if (LineEnsemble == null) return;
+
+            indexList[0] = BinarySearch(startWNum);
+            indexList[1] = BinarySearch(endWNum);
+
+            //if (!DebugMode) return;
+
+            //if (indexList[0] < 0 || indexList[1] < 0) return;
+        }
+
+        // Convenience method, currently deprecated
+        public static Tuple<double[,], double[,], double[,], double[,]> GetWidthTables(string species)
+        {
+            List<double> TempList = [];
+            for (double T = 300; T <= 6000; T += 300)
+            {
+                TempList.Add(T);
+            }
+
+            TempList.Insert(0, 100);
+            TempList.Insert(1, 200);
+
+            List<double> wNumList = [];
+            for (double wNum = 0; wNum < 30001; wNum += 100)
+            {
+                wNumList.Add(wNum);
+            }
+
+            double[,] minSelfWidthArray = new double[TempList.Count, wNumList.Count - 1];
+            double[,] maxSelfWidthArray = new double[TempList.Count, wNumList.Count - 1];
+            double[,] minAirWidthArray = new double[TempList.Count, wNumList.Count - 1];
+            double[,] maxAirWidthArray = new double[TempList.Count, wNumList.Count - 1];
+
+            for (int i = 0; i < TempList.Count; i++)
+            {
+                for (int j = 0; j < wNumList.Count - 1; j++)
+                {
+                    minSelfWidthArray[i, j] = double.PositiveInfinity;
+
+                    maxSelfWidthArray[i, j] = double.NegativeInfinity;
+
+                    minAirWidthArray[i, j] = double.PositiveInfinity;
+
+                    maxAirWidthArray[i, j] = double.NegativeInfinity;
+                }
+            }
+
+            DataBuffer dBuffer = new();
+
+            RunEngine.InitializeDataBuffer(dBuffer, species, 0, 30000);
+
+            dBuffer.UpdateData();
+
+            int wNumIx = 0;
+
+            while (dBuffer.LineEnsemble != null)
+            {
+                for (int i = 0; i < dBuffer.LineEnsemble.LineCount; i++)
+                {
+                    int lineCenterIx = i * LineEnsemble.NDataColumns;
+
+                    int TIx = 0;
+                    foreach (var T in TempList)
+                    {
+                        double lineCenter = dBuffer.LineEnsemble.DataArray[lineCenterIx];
+
+                        while (lineCenter > wNumList[wNumIx + 1])
+                        {
+                            wNumIx++;
+                        }
+
+                        double w = dBuffer.LineEnsemble.GetHalfWidth(i, 0, T);
+
+                        if (minAirWidthArray[TIx, wNumIx] > w) minAirWidthArray[TIx, wNumIx] = w;
+                        if (maxAirWidthArray[TIx, wNumIx] < w) maxAirWidthArray[TIx, wNumIx] = w;
+
+                        w = dBuffer.LineEnsemble.GetHalfWidth(i, 1, T);
+
+                        if (minSelfWidthArray[TIx, wNumIx] > w) minSelfWidthArray[TIx, wNumIx] = w;
+                        if (maxSelfWidthArray[TIx, wNumIx] < w) maxSelfWidthArray[TIx, wNumIx] = w;
+
+                        TIx++;
+                    }
+                }
+
+                dBuffer.UpdateData();
+            }
+
+            return new Tuple<double[,], double[,], double[,], double[,]>(minSelfWidthArray, maxSelfWidthArray,
+                minAirWidthArray, maxAirWidthArray);
+        }
+
+        public void Initialize(string folder, string pattern, double startWNum, double endWNum)
+        {
+            FolderName = folder;
+            FilePattern = pattern;
+            StartWavenumber = startWNum;
+            EndWavenumber = endWNum;
+
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            if (DebugMode)
+            {
+                if (File.Exists(DebugFileName)) File.Delete(DebugFileName);
+            }
+
+            _fileList = [];
+
+            // Get list of files which match the specified pattern, from the specified folder
+            string[] fileList = Directory.GetFiles(FolderName, FilePattern);
+
+            foreach (string f in fileList)
+            {
+                FileData newFData = new();
+                newFData.SetFileName(f);
+
+                _fileList.Add(newFData);
+            }
+
+            // Truncate file list to only contain files relevant to the specified wavenumber range
+            // The wavenumber range is augmented by the neighbor line cutoff
+            _fileList = [.._fileList.Where(item => item.StartWavenumber < EndWavenumber + NeighborLinesWNumCutoff
+            && item.EndWavenumber > StartWavenumber - NeighborLinesWNumCutoff)];
+
+            // List of starting wavenumbers for each data file
+            // For example: for CDSD, each file contains lines for a wavenumber range of
+            // several hundred cm-1; binary files are named according to the start wavenumber
+            // and end wavenumber of lines contained in the file
+            List<double> sWNumList = [.. _fileList.Select(item => item.StartWavenumber)];
+
+            // Reorder the file list by start wavenumbers in ascending order
+            // This step is usually redundant, since files are anyway presented
+            // in the order of start wavenumbers by the operating system
+            _fileList = sWNumList.Zip(_fileList, (val1, val2) => new { val1, val2 }).
+                OrderBy(pair => pair.val1).Select(pair => pair.val2).ToList();
+
+            _fileIndex = 0;
+        }
+
+        public override string ToString()
+        {
+            return FolderName + " (" + StartWavenumber + " to " + EndWavenumber + " cm-1)";
+        }
+
+        /// <summary>
+        /// This method is periodically called to pull data from the next file
+        /// Data are stored in the buffer within the "LineEnsemble" property
+        /// </summary>
+        public void UpdateData()
+        {
+            if (_fileList is null || _fileIndex >= _fileList.Count)
+            {
+                LineEnsemble = null;
+
+                return;
+            }
+
+            TimingFunctions.InitializeTime("RHTC: DataBuffer: GetData");
+
+            Log.Information($"Reading file: {Path.GetFileName(_fileList[_fileIndex].FileName)}");
+
+            LineEnsemble = new();
+
+            LineEnsemble.ReadFromBinaryFile(_fileList[_fileIndex].FileName);
+
+            _fileIndex++;
+
+            TimingFunctions.AddTime("RHTC: DataBuffer: GetData");
+        }
+
+        #endregion Public Methods
+
+        #region Public Properties
+
+        // For debugging only
+        public bool DebugMode
+        {
+            get { return _debugMode; }
+            set { _debugMode = value; }
+        }
+
+        public string? DebugFileName
+        {
+            get { return _debugFileName; }
+            set { _debugFileName = value; }
+        }
+
+        // Ending wavenumber of spectral interval (cm-1)
+        public double EndWavenumber
+        {
+            get { return _endWavenumber; }
+            set { _endWavenumber = value; }
+        }
+
+        // File name pattern for loading binary line data files
+        public string FilePattern
+        {
+            get { return _filePattern; }
+            set { _filePattern = value; }
+        }
+
+        // Base folder containing line data binary files
+        public string? FolderName
+        {
+            get { return _folderName; }
+            set { _folderName = value; }
+        }
+
+        // Holds line data in memory as flattened arrays
+        public LineEnsemble? LineEnsemble
+        {
+            get { return _lineEnsemble; }
+            set { _lineEnsemble = value; }
+        }
+
+        // For each spectral sub-interval, some lines outside the sub-interval
+        // are also considered, since these lines have overlap with the sub-interval
+        public double NeighborLinesWNumCutoff
+        {
+            get { return _neighborLinesWNumCutoff; }
+            set { _neighborLinesWNumCutoff = value; }
+        }
+
+        // Ex.: CDSD CO2 or HITRAN H2O
+        public string SpeciesDatabaseDef
+        {
+            get { return _speciesDatabaseDef; }
+            set { _speciesDatabaseDef = value; }
+        }
+
+        // Starting wavenumber of spectral interval (cm-1)
+        public double StartWavenumber
+        {
+            get { return _startWavenumber; }
+            set { _startWavenumber = value; }
+        }
+
+        #endregion Public Properties
+
+        #region Private Methods
+
+        /// <summary>
+        /// Implements a binary search to get the index of the file
+        /// corresponding to the given wavenumber location
+        /// </summary>
+        /// <param name="wNum">Wavenumber location in cm-1</param>
+        /// <returns></returns>
+        int BinarySearch(double wNum)
+        {
+            if (LineEnsemble is null) return -1;
+
+            double[] dataArray = LineEnsemble.DataArray;
+
+            int N = dataArray.Length / LineEnsemble.NDataColumns;
+            if (N == 0) return -1;
+
+            if (dataArray[0] >= wNum)
+            {
+                return -1;
+            }
+
+            if (dataArray[(N - 1) * LineEnsemble.NDataColumns] <= wNum) return N - 1;
+
+            int ix1 = 0, ix2 = N, ix = (ix1 + ix2) >> 1;
+
+            while (true)
+            {
+                int ixSingle = ix * LineEnsemble.NDataColumns;
+
+                if (dataArray[ixSingle] <= wNum && dataArray[(ix + 1) * LineEnsemble.NDataColumns] >= wNum) return ix;
+
+                if (dataArray[ixSingle] > wNum)
+                {
+                    ix2 = ix;
+
+                    ix = (ix2 + ix1) >> 1;
+                }
+                else if (dataArray[ixSingle] < wNum)
+                {
+                    ix1 = ix;
+
+                    ix = (ix1 + ix2) >> 1;
+                }
+            }
+        }
+
+        #endregion Private Methods
+
+        #region Private Properties
+
+        bool _debugMode = false;
+
+        string? _debugFileName = null;
+
+        // Ending wavenumber of spectral interval (cm-1)
+        double _endWavenumber = 10000;
+
+        // Internal book-keeping
+        int _fileIndex = 0;
+
+        // Description of files to be considered for the run
+        List<FileData>? _fileList = null;
+
+        // File name pattern for loading binary line data files
+        string _filePattern = "";
+
+        // Base folder containing line data binary files
+        string? _folderName = null;
+
+        // Holds line data in memory as flattened arrays
+        LineEnsemble? _lineEnsemble = null;
+
+        // For each spectral sub-interval, some lines outside the sub-interval
+        // are also considered, since these lines have overlap with the sub-interval
+        double _neighborLinesWNumCutoff = 0;
+
+        // Ex.: CDSD CO2 or HITRAN H2O
+        string _speciesDatabaseDef = "";
+
+        // Starting wavenumber of spectral interval (cm-1)
+        double _startWavenumber = 200;
+
+        #endregion Private Properties
+    }
+}
---
+++ LineEnsemble.cs
@@ -0,0 +1,338 @@
+﻿using Serilog;
+
+namespace DCTRClasses
+{
+    public class LineEnsemble
+    {
+        #region Public Methods
+
+        public void AppendToFile(string fileName, int startIx, int endIx, double intensityCutoff)
+        {
+            int curIx = startIx * NDataColumns;
+
+            using StreamWriter sw = new(fileName, true);
+            for (int i = startIx; i < endIx; i++)
+            {
+                if (curIx >= 0 && DataArray[curIx + 1] >= intensityCutoff)
+                {
+                    for (int j = 0; j < NDataColumns; j++)
+                    {
+                        sw.Write(DataArray[curIx + j]);
+                        if (j < NDataColumns - 1) sw.Write("\t");
+                    }
+
+                    sw.WriteLine();
+                }
+
+                curIx += NDataColumns;
+            }
+        }
+
+        /// <summary>
+        /// Gets the composite half-width (self and air-broadened, weighted by mole fraction)
+        /// </summary>
+        /// <param name="ix">Row index (specifies the line which is being considered)</param>
+        /// <param name="moleFraction"></param>
+        /// <param name="T">In Deg. K</param>
+        /// <returns></returns>
+        public double GetHalfWidth(int ix, double moleFraction, double T)
+        {
+            int ixSingle = ix * NDataColumns;
+
+            double value = 0, v, f;
+            T = TRef / T;
+
+            v = (1 - moleFraction) * DataArray[ixSingle + 4];
+            f = Math.Pow(T, DataArray[ixSingle + 6]);
+            value += v * f;
+
+            v = moleFraction * DataArray[ixSingle + 5];
+            f = Math.Pow(T, DataArray[ixSingle + 7]);
+            value += v * f;
+
+            return value;
+        }
+
+        public static int GetIndex(int lineIndex, string parameter)
+        {
+            return lineIndex * NDataColumns + DataColumnDict[parameter];
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="N">Total number of lines</param>
+        public void Initialize(int N)
+        {
+            // 0: Center
+            // 1: Intensity
+            // 2: Lower energy
+            // 3: Pressure shift
+            // 4: wAir
+            // 5: wSelf
+            // 6: nAir
+            // 7: nSelf
+            // 8: Current center
+            // 9: Current intensity
+            // 10: Current half-width
+            // 11: Raw scaled intensity
+            DataArray = new double[N * NDataColumns];
+
+            // 0: Wavenumber index (calculated)
+            // 1: Width index (calculated)
+            // 2: Isotope ID (from source)
+            IndexArray = new int[N * NIndexColumns];
+
+            LineCount = N;
+        }
+
+        public void ReadFromBinaryFile(string fileName)
+        {
+            if (fileName.Contains("CDSD")) TRef = 300;
+            else TRef = 296;
+
+            LineCount = (int)(new FileInfo(fileName).Length / 68);
+
+            using BinaryReader br = new(File.Open(fileName, FileMode.Open));
+            Initialize(LineCount);
+
+            int lCount = LineCount * NDataColumns;
+
+            int ix = 0;
+            for (int i = 0; i < lCount; i += NDataColumns)
+            {
+                DataArray[i] = br.ReadDouble();
+                DataArray[i + 1] = br.ReadDouble();
+                DataArray[i + 2] = br.ReadDouble();
+                DataArray[i + 3] = br.ReadDouble();
+                DataArray[i + 4] = br.ReadDouble();
+                DataArray[i + 5] = br.ReadDouble();
+                DataArray[i + 6] = br.ReadDouble();
+                DataArray[i + 7] = br.ReadDouble();
+
+                IndexArray[ix + 2] = br.ReadInt32();
+
+                ix += NIndexColumns;
+            }
+        }
+
+        public void ReadFromFile(string fileName)
+        {
+            // HITEMP
+            List<int> countList = [2, 1, 12, 10, 10, 5];
+            countList.AddRange([5, 10, 4, 8, 4]);
+
+            List<double[]> arrayList = [];
+            List<int> indexArrayList = [];
+
+            string? l;
+            using (StreamReader sr = new(fileName))
+            {
+                l = sr.ReadLine();
+
+                while (l != null)
+                {
+                    var splitList = StringFunctions.SplitString(l, countList);
+
+                    double lineCenter = Convert.ToDouble(splitList[2]);
+                    double lineIntensity = Convert.ToDouble(splitList[3]);
+
+                    double wAir = Convert.ToDouble(splitList[5]);
+                    double wSelf = Convert.ToDouble(splitList[6]);
+                    double lowerEnergy = Convert.ToDouble(splitList[7]);
+                    double nAir = Convert.ToDouble(splitList[8]);
+                    double pressureShift = Convert.ToDouble(splitList[9]);
+
+                    double nSelf;
+
+                    int isotopeID;
+
+                    if (splitList[1] == "0") isotopeID = 10;
+                    else if (splitList[1] == "A") isotopeID = 11;
+                    else if (splitList[1] == "B") isotopeID = 12;
+                    else isotopeID = Convert.ToInt32(splitList[1]);
+
+                    if (splitList.Count > 10)
+                    {
+                        bool b = double.TryParse(splitList[10], out double v);
+                        nSelf = b ? v : nAir;
+                    }
+                    else
+                    {
+                        nSelf = nAir;
+                    }
+
+                    arrayList.Add([lineCenter, lineIntensity, lowerEnergy, pressureShift, wAir, wSelf, nAir, nSelf]);
+                    indexArrayList.Add(isotopeID);
+
+                    l = sr.ReadLine();
+                }
+            }
+
+            int N = arrayList.Count;
+
+            Initialize(N);
+
+            Log.Information($"Read {N} lines");
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    DataArray[i * NDataColumns + j] = arrayList[i][j];
+                }
+
+                IndexArray[i * NIndexColumns + 2] = indexArrayList[i];
+            }
+        }
+
+        /// <summary>
+        /// Sets parameters such as scaled intensity, composite half-width, etc.
+        /// </summary>
+        /// <param name="ix">Row index (represents individual lines)</param>
+        /// <param name="state">Contains specifications of each homogeneous slab within the inhomogeneous path</param>
+        /// <param name="species">Gas species</param>
+        /// <param name="startWNum"></param>
+        /// <param name="widthRanges"></param>
+        /// <param name="widthRatio"></param>
+        /// <param name="wNumRes"></param>
+        /// <param name="intensityCutoff">Line intensity threshold</param>
+        public void SetCurrentState(int ix, RunEngineState state, string species, double startWNum
+    , double[] widthRanges, double widthRatio,
+    double wNumRes, double intensityCutoff)
+        {
+            int ixSingle = ix * NDataColumns, ixIndex = ix * NIndexColumns;
+
+            int isotopeID = IndexArray[ixIndex + 2];
+
+            double LEC2 = DataArray[ixSingle + 2] * c2;
+            double LCC2 = -DataArray[ixSingle] * c2;
+            double OnesComplementCNuTRefInv = DataArray[ixSingle + 1] / (1 - Math.Exp(LCC2 / TRef));
+
+            double v = (1 - Math.Exp(LCC2 / state.Temperature)) * OnesComplementCNuTRefInv;
+            v *= Math.Exp(LEC2 * (1 / TRef - 1 / state.Temperature));
+
+            int pfIsotopeID = isotopeID;
+            if (!state.PartitionFunctionValueDictionary[species].ContainsKey(pfIsotopeID)) { pfIsotopeID = 1; }
+
+            v /= state.PartitionFunctionValueDictionary[species][pfIsotopeID];
+
+            // Raw scaled intensity
+            DataArray[ixSingle + 11] = v;
+
+            if (v < intensityCutoff) return;
+
+            // Scaled intensity
+            DataArray[ixSingle + 9] = v * state.IntensityScalingFactor;
+
+            // Pressure shift of line center
+            double Delta0 = DataArray[ixSingle + 3] * state.TotalPressure * (1 - state.MoleFraction);
+
+            // Pressure-shifted line center
+            DataArray[ixSingle + 8] = DataArray[ixSingle] + Delta0;
+
+            // Line half-width
+            DataArray[ixSingle + 10] = state.TotalPressure * GetHalfWidth(ix, state.MoleFraction, state.Temperature);
+
+            // This calculates the index of the wavenumber location in the output array
+            IndexArray[ixIndex] = (int)((DataArray[ixSingle + 8] -
+                startWNum) / wNumRes);
+
+            // This calculates the representative Lorentz half-width bin index
+            if (widthRanges == null)
+            {
+                IndexArray[ixIndex + 1] = -1;
+            }
+            else
+            {
+                int w = (int)(Math.Log(DataArray[ixSingle + 10] / widthRanges[0])
+                    / Math.Log(widthRatio));
+
+                if (w >= widthRanges.Length)
+                {
+                    w = widthRanges.Length - 1;
+                }
+
+                IndexArray[ixIndex + 1] = w;
+
+                if (IndexArray[ixIndex + 1] < 0) IndexArray[ixIndex + 1] = 0;
+            }
+        }
+
+        public void WriteBinaryFile(string fileName)
+        {
+            using BinaryWriter bw = new(File.Open(fileName, FileMode.Create));
+            for (int i = 0; i < LineCount; i++)
+            {
+                int ix = i * NDataColumns;
+
+                for (int j = 0; j < 8; j++) bw.Write(DataArray[ix + j]);
+                bw.Write(IndexArray[i * NIndexColumns + 2]);
+            }
+        }
+
+        #endregion Public Methods
+
+        #region Public Properties
+
+        // Dictionary which specifies the column index of each line parameter
+        // Key represents the line parameter; value represents the column index
+        readonly static Dictionary<string, int> DataColumnDict = [];
+
+        public int LineCount = 0;
+
+        // Static array which contains line data; rows represent individual lines,
+        // columns represent line parameters; the 2-d array is flattened into a 1-d array
+        // for fast access
+        public double[]? DataArray = null;
+
+        // Static array which contains line indices (see description of "NIndexColumns" above)
+        // Rows represent individual lines, columns represent line indices; the 2-d array is flattened
+        // into a 1-d array for fast access
+        public int[]? IndexArray = null;
+
+        // Reference temperature (K)
+        public double TRef = 296;
+
+        #endregion Public Properties
+
+        #region Private Methods
+
+        static LineEnsemble()
+        {
+            DataColumnDict.Add("LineCenter", 0);
+            DataColumnDict.Add("LineIntensity", 1);
+            DataColumnDict.Add("LowerEnergy", 2);
+            DataColumnDict.Add("PressureShift", 3);
+            DataColumnDict.Add("AirWidth", 4);
+            DataColumnDict.Add("SelfWidth", 5);
+            DataColumnDict.Add("AirIndex", 6);
+            DataColumnDict.Add("SelfIndex", 7);
+            DataColumnDict.Add("ShiftedCenter", 8);
+            DataColumnDict.Add("ScaledIntensity", 9);
+            DataColumnDict.Add("ScaledWidth", 10);
+            DataColumnDict.Add("RawScaledIntensity", 11);
+        }
+
+        #endregion Private Methods
+
+        #region Private Properties
+
+        // Radiation constant
+        const double c2 = 1.4388028496642257;
+
+        // Avogadro number (molecules/mol)
+        const double NA = 6.02214076e23;
+
+        // NDataColumns is the total number of columns which represents each line
+        // Each column contains one particular line parameter, such as intensity, line center, etc.
+        // NIndexColumns contains useful calculated indices, such as wavenumber location,
+        // or source indices, such as isotope ID
+        public static readonly int NDataColumns = 12, NIndexColumns = 3;
+
+        // Universal gas constant (J/mol-K)
+        const double R = 8.314;
+
+        #endregion Private Properties
+    }
+}
